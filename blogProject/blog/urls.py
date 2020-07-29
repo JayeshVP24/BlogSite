0d80d373c724af894deb00bf08dd6b524954aade
Mr-Jayesh <jayeshpotlabattini@gmail.com>
2020-07-29 18:18:32 +0530

    Created draft list view

--- blogProject/blog/urls.py
+++ blogProject/blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('post/new/',views.PostCreateView.as_view(), name='post_new'),
     re_path(r'^post(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
     re_path(r'^post/(?P,pk.\d+)/remove/',views.PostDeleteView.as_view(),name='post_remove'),
+    path('drafts/',views.DraftListView.as_view(),name='post_draft_list'),
 ]
 
--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -14,7 +14,7 @@ class PostListView(ListView):
     model = Post
     
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
     model = Post
@@ -34,3 +34,12 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, DeleteViews):
     model = Post
     success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
