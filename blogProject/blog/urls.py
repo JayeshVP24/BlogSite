e7e5735fb2063913b1a3be8065ce81a221a84ec0
Mr-Jayesh <jayeshpotlabattini@gmail.com>
2020-07-29 18:08:50 +0530

    Created CRUD Views of POST

--- blogProject/blog/urls.py
+++ blogProject/blog/urls.py
@@ -1,7 +1,14 @@
+from blogProject.blog.views import PostCreateView, PostListView
 from blogProject.blogProject.urls import urlpatterns
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
     path('about/',views.AboutView.as_view(),name='about'),
+    re_path(r'^post/(?P<pk>\d+)$',views.PostDetailView.as_view(),name='post_detail'),
+    path('post/new/',views.PostCreateView.as_view(), name='post_new'),
+    re_path(r'^post(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
+    re_path(r'^post/(?P,pk.\d+)/remove/',views.PostDeleteView.as_view(),name='post_remove'),
 ]
+
--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.utils import timezone
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView,ListView,DetailView,CreateView, UpdateView, DeleteViews
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
 # Create your views here.
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+    
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm    
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm    
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin, DeleteViews):
+    model = Post
+    success_url = reverse_lazy('post_list')
